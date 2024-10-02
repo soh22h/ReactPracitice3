e3c14d6b05907bde856c7ee79f578bac1047221f
윤영헌 <yun-yeongheon@yun-yeongheon-ui-MacBookPro.local>
2024-10-02 13:18:55 +0900

    rempve footer:

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import "./App.css";
 // 기타 필요한 컴포넌트 임포트
@@ -16,7 +15,6 @@ const App: React.FC = () => {
         <Route path="/" element={<MainContent />} />
         {/* 다른 페이지 라우트 설정 */}
       </Routes>
-      <Footer />
     </Router>
   );
 };
