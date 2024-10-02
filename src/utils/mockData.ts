81636022d3e5f940a5b13e8f680a4b01325797ca
soh22h <soh22h@gmail.com>
2024-10-02 13:54:40 +0900

    add utils

---
+++ src/utils/mockData.ts
@@ -0,0 +1,50 @@
+import { Product } from "./src/utils/type";
+
+const sampleProducts: Product[] = [
+  {
+    id: 1,
+    name: "",
+    price: 50000,
+    imageUrl: "",
+  },
+  {
+    id: 2,
+    name: "",
+    price: 75000,
+    imageUrl: "",
+  },
+  {
+    id: 1,
+    name: "",
+    price: 50000,
+    imageUrl: "",
+  },
+  {
+    id: 2,
+    name: "",
+    price: 75000,
+    imageUrl: "",
+  },  {
+    id: 1,
+    name: "",
+    price: 50000,
+    imageUrl: "",
+  },
+  {
+    id: 2,
+    name: "",
+    price: 75000,
+    imageUrl: "",
+  },  {
+    id: 1,
+    name: "",
+    price: 50000,
+    imageUrl: "",
+  },
+  {
+    id: 1,
+    name: "",
+    price: 50000,
+    imageUrl: "",
+  }
+];
