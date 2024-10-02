30dda714c7e2fe6e52b4e3ec7f393aa9ab20674d
htchoi1006 <htchoi1006@kookmin.ac.kr>
2024-10-02 14:59:10 +0900

    헤더파일 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,59 +1,82 @@
-// src/components/Header.tsx
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/Header.css";
+import { Search, User, Heart, ShoppingBag } from "lucide-react";
+import "../App.css";
+import logo from "./logo.jpg";
 
 const Header: React.FC = () => {
-  return (
-    <header className="header">
-      {/* 상단 바 */}
-      <div className="header__top">
-        {/* 로고 */}
-        <div className="header__logo">
-          <Link to="/">
-            <img
-              src="https://via.placeholder.com/150x50?text=Logo"
-              alt="Logo"
-            />
-          </Link>
-        </div>
+	return (
+		<header className="relative w-full bg-white text-black border-b border-gray-200 h-24">
+			<div className="flex items-center justify-between px-4 h-full">
+				{/* Logo */}
+				<div className="flex-shrink-0">
+					<Link to="/">
+						<img
+							src={logo}
+							alt="Adidas logo"
+							className="h-20 w-auto object-contain"
+						/>
+					</Link>
+				</div>
 
-        {/* 검색창 */}
-        <div className="header__search">
-          <input type="text" placeholder="검색어를 입력하세요" />
-          <button type="submit">검색</button>
-        </div>
+				{/* User Menu */}
+				<div className="flex items-center space-x-4">
+					<div className="relative">
+						<input
+							type="text"
+							placeholder="Search"
+							className="pl-8 pr-2 py-2 bg-gray-100 rounded-full text-sm w-48"
+						/>
+						<Search className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
+					</div>
+					<User className="w-6 h-6" />
+					<div className="relative">
+						<Heart className="w-6 h-6" />
+						<span className="absolute -top-1 -right-1 bg-yellow-400 text-xs rounded-full w-4 h-4 flex items-center justify-center">
+							1
+						</span>
+					</div>
+					<ShoppingBag className="w-6 h-6" />
+				</div>
+			</div>
 
-        {/* 사용자 메뉴 */}
-        <div className="header__user-menu">
-          <Link to="/login">로그인</Link>
-          <Link to="/register">회원가입</Link>
-          <Link to="/cart">장바구니</Link>
-        </div>
-      </div>
-
-      {/* 네비게이션 메뉴 */}
-      <nav className="header__nav">
-        <ul>
-          <li>
-            <Link to="/category/men">남성</Link>
-          </li>
-          <li>
-            <Link to="/category/women">여성</Link>
-          </li>
-          <li>
-            <Link to="/category/accessories">액세서리</Link>
-          </li>
-          <li>
-            <Link to="/category/shoes">신발</Link>
-          </li>
-          <li>
-            <Link to="/sale">세일</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+			{/* Centered Navigation */}
+			<nav className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
+				<ul className="flex justify-center space-x-8">
+					<li>
+						<Link to="/men" className="font-bold text-lg">
+							MEN
+						</Link>
+					</li>
+					<li>
+						<Link to="/women" className="font-bold text-lg">
+							WOMEN
+						</Link>
+					</li>
+					<li>
+						<Link to="/kids" className="font-bold text-lg">
+							KIDS
+						</Link>
+					</li>
+					<li>
+						<Link to="/sports" className="text-lg">
+							SPORTS
+						</Link>
+					</li>
+					<li>
+						<Link to="/brands" className="text-lg">
+							BRANDS
+						</Link>
+					</li>
+					<li>
+						<Link to="/outlet" className="text-lg">
+							OUTLET
+						</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
