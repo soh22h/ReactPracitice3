a39782352adc922ad40f02977c6cce5775a7d0b1
magae1 <lws6665@naver.com>
2024-10-02 16:38:48 +0900

    💄 쇼핑 카드

--- src/components/main-components/card.tsx
+++ src/components/main-components/card.tsx
@@ -1,12 +1,24 @@
 import { Product } from "../../utils/type";
-interface CardProps{
-  sampleProducts: Product[]
+import { useAppSelector } from "../../redux/store.ts";
+import useLocalStorage from "../../hooks/useLocationStorage.ts";
+import { useCallback } from "react";
+interface CardProps {
+  sampleProducts: Product[];
 }
-export function Card({sampleProducts}:CardProps){
-  
+export function Card({ sampleProducts }: CardProps) {
+  const loginData = useAppSelector((state) => state.user.loginUser);
+  const [_, setCart] = useLocalStorage<Product[]>(
+    loginData ? loginData.id : "",
+    [],
+  );
+
+  const onAddCart = useCallback((product: Product) => {
+    setCart((prev) => [...prev, product]);
+  }, []);
+
   return (
-  <>
-  <div className="product-grid">
+    <>
+      <div className="product-grid">
         {sampleProducts.map((product) => (
           <div className="product-card" key={product.id}>
             <img src={product.imageUrl} alt={product.imageUrl} />
@@ -14,10 +26,12 @@ export function Card({sampleProducts}:CardProps){
             <p className="product-card__price">
               {product.price.toLocaleString()}원
             </p>
-            <button className="button">add to cart</button>
+            <button className="button" onClick={() => onAddCart(product)}>
+              add to cart
+            </button>
           </div>
         ))}
-    </div>
-  </>
-  )
+      </div>
+    </>
+  );
 }
